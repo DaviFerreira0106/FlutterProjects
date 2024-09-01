87ffcc0cbd13f173f8d4ec86121441bd981c270a
Davi Ferreira <123248131+DaviFerreira0106@users.noreply.github.com>
2024-09-01 17:25:02 -0300

    Confecção de tela do app com botoes

--- projeto_perguntas/lib/main.dart
+++ projeto_perguntas/lib/main.dart
@@ -2,8 +2,11 @@ import 'package:flutter/material.dart';
 
 void main() => runApp(PerguntaApp());
 
-
 class PerguntaApp extends StatelessWidget {
+  final List<String> perguntas = [
+    'Qual sua cor favorita ?',
+    'Qual seu animal favorito ?',
+  ];
 
   @override
   Widget build(BuildContext context) {
@@ -12,7 +15,23 @@ class PerguntaApp extends StatelessWidget {
         appBar: AppBar(
           title: const Text('Perguntas'),
         ),
-        body: const Text('Olá Mundo!!!!'),
+        body: Column(
+          children: <Widget>[
+            Text(perguntas.elementAt(0)),
+            ElevatedButton(
+              onPressed: null,
+              child: Text('Resposta 1'),
+            ),
+            ElevatedButton(
+              onPressed: null,
+              child: Text('Resposta 2'),
+            ),
+            ElevatedButton(
+              onPressed: null,
+              child: Text('Resposta 3'),
+            ),
+          ],
+        ),
       ),
     );
   }
