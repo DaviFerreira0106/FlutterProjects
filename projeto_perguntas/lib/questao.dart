9f8e4a5b818550d193698540c10a9c925f853b66
Davi Ferreira <123248131+DaviFerreira0106@users.noreply.github.com>
2024-09-08 22:55:05 -0300

    Atualização de conclusao

--- projeto_perguntas/lib/questao.dart
+++ projeto_perguntas/lib/questao.dart
@@ -3,7 +3,7 @@ import 'package:flutter/material.dart';
 class Questao extends StatelessWidget {
   final String? texto;
 
-  Questao({super.key, this.texto});
+  const Questao({super.key, this.texto});
 
   @override
   Widget build(BuildContext context) {
