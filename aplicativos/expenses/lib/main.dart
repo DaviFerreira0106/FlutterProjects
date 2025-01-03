2e19e742bf23ed1d72252095cb10312441faaf07
Davi Ferreira <123248131+DaviFerreira0106@users.noreply.github.com>
2025-01-02 21:05:55 -0300

    Criando componentes do aplicativo

--- aplicativos/expenses/lib/main.dart
+++ aplicativos/expenses/lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:expenses/models/transaction.dart';
 import 'package:flutter/material.dart';
 
 void main() => runApp(ExpensesApp());
@@ -14,17 +15,77 @@ class ExpensesApp extends StatelessWidget {
 }
 
 class MyHomePage extends StatelessWidget {
-  const MyHomePage({super.key});
+  final List<Transaction> _transaction = [
+    Transaction(
+      id: "t1",
+      title: "Novo Tênis de Corrida",
+      value: 380.80,
+      date: DateTime.now(),
+    ),
+    Transaction(
+      id: "t2",
+      title: "Conta de luz",
+      value: 211.60,
+      date: DateTime.now(),
+    )
+  ];
+
+  MyHomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text("Despesas Pessoais"),
-      ),
-      body: Center(
-        child: Text("Versão Inicial"),
-      ),
-    );
+        appBar: AppBar(
+          title: Text("Despesas Pessoais"),
+        ),
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Container(
+              child: Card(
+                color: Colors.blue,
+                child: Text("Gráfico"),
+              ),
+            ),
+            Column(
+              children: _transaction.map((tr) {
+                return Card(
+                  child: Row(
+                    children: [
+                      Container(
+                        margin: EdgeInsets.symmetric(
+                          horizontal: 15,
+                          vertical: 10,
+                        ),
+                        decoration: BoxDecoration(
+                          border: Border.all(
+                            color: Colors.purple,
+                            width: 2,
+                          ),
+                        ),
+                        padding: EdgeInsets.all(10),
+                        child: Text(
+                          tr.value.toString(),
+                          style: TextStyle(
+                            color: Colors.purple,
+                            fontWeight: FontWeight.bold,
+                            fontSize: 20,
+                          ),
+                        ),
+                      ),
+                      Column(
+                        children: [
+                          Text(tr.title),
+                          Text(tr.date.toString()),
+                        ],
+                      )
+                    ],
+                  ),
+                );
+              }).toList(),
+            )
+          ],
+        ));
   }
 }
