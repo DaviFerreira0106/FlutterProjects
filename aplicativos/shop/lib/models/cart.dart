dd247a118648e522864f660009fcecd7babc23bb
Davi Ferreira <123248131+DaviFerreira0106@users.noreply.github.com>
2025-03-19 11:28:40 -0300

    Criação de classes de modelo para o carrinho
    Criação da lógica das classes de modelo para o carrinho de compra

---
+++ aplicativos/shop/lib/models/cart.dart
@@ -0,0 +1,58 @@
+import 'package:flutter/material.dart';
+import 'package:shop/models/cart_item.dart';
+import 'package:shop/models/product.dart';
+import 'dart:math';
+
+class Cart with ChangeNotifier {
+  Map<String, CartItem> _itemsCart = {};
+
+  Map<String, CartItem> get itemsCart => {..._itemsCart};
+
+  int get itemsCartCount => _itemsCart.length;
+
+  double get totalAmount {
+    double total = 0.0;
+
+    _itemsCart.forEach(
+      (idItemsCart, cartItem) => total += cartItem.price * cartItem.quandity,
+    );
+    return total;
+  }
+
+  void addItemCart(Product product) {
+    if (_itemsCart.containsKey(product.id)) {
+      _itemsCart.update(
+        product.id,
+        (existingCartItem) => CartItem(
+          id: existingCartItem.id,
+          productId: existingCartItem.productId,
+          quandity: existingCartItem.quandity + 1,
+          title: existingCartItem.title,
+          price: existingCartItem.price,
+        ),
+      );
+    } else {
+      _itemsCart.putIfAbsent(
+        product.id,
+        () => CartItem(
+          id: Random().nextDouble().toString(),
+          productId: product.id,
+          quandity: 1,
+          title: product.title,
+          price: product.price,
+        ),
+      );
+    }
+    notifyListeners();
+  }
+
+  void removeItemCart(String productId) {
+    _itemsCart.remove(productId);
+    notifyListeners();
+  }
+
+  void clear() {
+    _itemsCart = {};
+    notifyListeners();
+  }
+}
