332e85155ee51f4acea01bfc4a9aeb99bb50ff8c
Davi Ferreira <123248131+DaviFerreira0106@users.noreply.github.com>
2025-03-27 15:19:58 -0300

    Conclusão da tela de pedidos

---
+++ aplicativos/shop/lib/components/order_widget.dart
@@ -0,0 +1,77 @@
+import 'package:flutter/material.dart';
+import 'package:shop/models/order.dart';
+import 'package:intl/intl.dart';
+
+class OrderWidget extends StatefulWidget {
+  final Order order;
+  const OrderWidget({
+    super.key,
+    required this.order,
+  });
+
+  @override
+  OrderWidgetState createState() {
+    return OrderWidgetState();
+  }
+}
+
+class OrderWidgetState extends State<OrderWidget> {
+  bool _expanded = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      child: Column(
+        children: [
+          ListTile(
+            title: Text("R\$ ${widget.order.total.toStringAsFixed(2)}"),
+            subtitle: Text(
+              DateFormat('dd/MM/yyyy     hh:mm').format(widget.order.date),
+            ),
+            trailing: IconButton(
+              onPressed: () {
+                setState(() => _expanded = !_expanded);
+              },
+              icon: Icon(
+                Icons.expand_more,
+              ),
+            ),
+          ),
+          if (_expanded)
+            Container(
+              height: (widget.order.products.length * 25.0) + 10,
+              padding: const EdgeInsets.symmetric(
+                horizontal: 10,
+                vertical: 4,
+              ),
+              child: ListView(
+                children: widget.order.products
+                    .map(
+                      (product) => Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Text(
+                            product.title,
+                            style: TextStyle(
+                              fontSize: 18,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                          Text(
+                            "${product.quandity}x  ${product.price}",
+                            style: TextStyle(
+                                fontSize: 18,
+                                fontWeight: FontWeight.bold,
+                                color: Colors.grey),
+                          ),
+                        ],
+                      ),
+                    )
+                    .toList(),
+              ),
+            ),
+        ],
+      ),
+    );
+  }
+}
--- aplicativos/shop/lib/pages/order_page.dart
+++ aplicativos/shop/lib/pages/order_page.dart
@@ -1,14 +1,28 @@
 import 'package:flutter/material.dart';
+import 'package:shop/components/app_drawer.dart';
+import 'package:shop/models/order_list.dart';
+import 'package:provider/provider.dart';
+import 'package:shop/components/order_widget.dart';
 
 class OrderPage extends StatelessWidget {
-  const OrderPage({super.key});
+  const OrderPage({
+    super.key,
+  });
 
   @override
   Widget build(BuildContext context) {
+    final OrderList orderList = Provider.of(context);
     return Scaffold(
       appBar: AppBar(
         title: Text("Meus Pedidos"),
       ),
+      body: ListView.builder(
+        itemCount: orderList.itemCount,
+        itemBuilder: (context, index) => OrderWidget(
+          order: orderList.listCart.elementAt(index),
+        ),
+      ),
+      drawer: AppDrawer(),
     );
   }
 }
