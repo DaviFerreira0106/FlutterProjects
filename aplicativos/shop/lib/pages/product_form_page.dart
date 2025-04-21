bf344c122c77a14e3d0eb18fd1664727b364efe2
Davi Ferreira <123248131+DaviFerreira0106@users.noreply.github.com>
2025-04-21 17:48:08 -0300

    Criando a validação do formulário

--- aplicativos/shop/lib/pages/product_form_page.dart
+++ aplicativos/shop/lib/pages/product_form_page.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:shop/models/product.dart';
+import 'dart:math';
 
 class ProductFormPage extends StatefulWidget {
   const ProductFormPage({super.key});
@@ -17,6 +19,12 @@ class ProductFormPageState extends State<ProductFormPage> {
 
   // Variável de controle de texto
   final TextEditingController _imageUrlController = TextEditingController();
+
+  // Chave do formulário
+  final _formKey = GlobalKey<FormState>();
+
+  // Objeto de armazenamento dos dados do formulário
+  final _formData = Map<String, Object>();
 
   @override
   void initState() {
@@ -37,15 +45,36 @@ class ProductFormPageState extends State<ProductFormPage> {
     setState(() {});
   }
 
+  void submitForm() {
+    _formKey.currentState?.validate();
+
+    _formKey.currentState?.save();
+
+    final Product newProduct = Product(
+      id: Random().nextDouble().toString(),
+      title: _formData['name'] as String,
+      description: _formData['description'] as String,
+      price: _formData['price'] as double,
+      imageUrl: _formData['imageUrl'] as String,
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text("Formulário de Produto"),
+        actions: [
+          IconButton(
+            icon: Icon(Icons.save),
+            onPressed: () => submitForm(),
+          ),
+        ],
       ),
       body: Padding(
         padding: const EdgeInsets.all(15),
         child: Form(
+          key: _formKey,
           child: ListView(
             children: [
               TextFormField(
@@ -54,6 +83,20 @@ class ProductFormPageState extends State<ProductFormPage> {
                 textInputAction: TextInputAction.next,
                 onFieldSubmitted: (_) =>
                     FocusScope.of(context).requestFocus(_priceFocus),
+                onSaved: (name) => _formData['name'] = name ?? '',
+                validator: (valueName) {
+                  final String name = valueName ?? '';
+
+                  if (name.trim().isEmpty) {
+                    return "Nome é obrigatório!";
+                  }
+
+                  if (name.trim().length < 3) {
+                    return "O nome precisa ter no mínimo 3 letras!";
+                  }
+
+                  return null;
+                },
               ),
               TextFormField(
                 decoration: InputDecoration(labelText: "Preço"),
@@ -62,12 +105,16 @@ class ProductFormPageState extends State<ProductFormPage> {
                 focusNode: _priceFocus,
                 onFieldSubmitted: (_) =>
                     FocusScope.of(context).requestFocus(_descriptionFocus),
+                onSaved: (price) =>
+                    _formData['price'] = double.parse(price ?? '0'),
               ),
               TextFormField(
                 decoration: InputDecoration(labelText: "Descrição"),
                 focusNode: _descriptionFocus,
                 keyboardType: TextInputType.multiline,
                 maxLines: 3,
+                onSaved: (description) =>
+                    _formData['description'] = description ?? '',
               ),
               Row(
                 crossAxisAlignment: CrossAxisAlignment.end,
@@ -76,8 +123,12 @@ class ProductFormPageState extends State<ProductFormPage> {
                     child: TextFormField(
                       decoration: InputDecoration(labelText: "Url da Imagem"),
                       focusNode: _imageUrlFocus,
+                      textInputAction: TextInputAction.done,
                       keyboardType: TextInputType.url,
                       controller: _imageUrlController,
+                      onFieldSubmitted: (_) => submitForm(),
+                      onSaved: (imageUrl) =>
+                          _formData['imageUrl'] = imageUrl ?? '',
                     ),
                   ),
                   Container(
