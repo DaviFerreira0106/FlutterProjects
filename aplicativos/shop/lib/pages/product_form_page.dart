3e57848b9920edfb2a78435c7289067a0605f003
Davi Ferreira <123248131+DaviFerreira0106@users.noreply.github.com>
2025-04-21 14:24:25 -0300

    Atualização da página de formulário
    Criação de campo de url da imagem e preview da imagem

--- aplicativos/shop/lib/pages/product_form_page.dart
+++ aplicativos/shop/lib/pages/product_form_page.dart
@@ -10,14 +10,31 @@ class ProductFormPage extends StatefulWidget {
 }
 
 class ProductFormPageState extends State<ProductFormPage> {
+  // Variáveis de controle de foco
   final FocusNode _priceFocus = FocusNode();
   final FocusNode _descriptionFocus = FocusNode();
+  final FocusNode _imageUrlFocus = FocusNode();
+
+  // Variável de controle de texto
+  final TextEditingController _imageUrlController = TextEditingController();
+
+  @override
+  void initState() {
+    super.initState();
+    _imageUrlFocus.addListener(updateImageUrl);
+  }
 
   @override
   void dispose() {
     super.dispose();
     _priceFocus.dispose();
     _descriptionFocus.dispose();
+    _imageUrlFocus.removeListener(updateImageUrl);
+    _imageUrlFocus.dispose();
+  }
+
+  void updateImageUrl() {
+    setState(() {});
   }
 
   @override
@@ -33,6 +50,7 @@ class ProductFormPageState extends State<ProductFormPage> {
             children: [
               TextFormField(
                 decoration: InputDecoration(labelText: "Nome"),
+                keyboardType: TextInputType.name,
                 textInputAction: TextInputAction.next,
                 onFieldSubmitted: (_) =>
                     FocusScope.of(context).requestFocus(_priceFocus),
@@ -50,6 +68,42 @@ class ProductFormPageState extends State<ProductFormPage> {
                 focusNode: _descriptionFocus,
                 keyboardType: TextInputType.multiline,
                 maxLines: 3,
+              ),
+              Row(
+                crossAxisAlignment: CrossAxisAlignment.end,
+                children: [
+                  Expanded(
+                    child: TextFormField(
+                      decoration: InputDecoration(labelText: "Url da Imagem"),
+                      focusNode: _imageUrlFocus,
+                      keyboardType: TextInputType.url,
+                      controller: _imageUrlController,
+                    ),
+                  ),
+                  Container(
+                    width: 100,
+                    height: 100,
+                    margin: EdgeInsets.only(
+                      top: 10,
+                      left: 10,
+                    ),
+                    alignment: Alignment.center,
+                    decoration: BoxDecoration(
+                      border: Border.all(
+                        color: Colors.grey,
+                        width: 1,
+                      ),
+                    ),
+                    child: _imageUrlController.text.isEmpty
+                        ? Text("Informe a Url")
+                        : FittedBox(
+                            fit: BoxFit.cover,
+                            child: Image.network(
+                              _imageUrlController.text,
+                            ),
+                          ),
+                  ),
+                ],
               ),
             ],
           ),
