78de6fd6f7958172e8c02b4f199c94a2749f5418
Davi Ferreira <123248131+DaviFerreira0106@users.noreply.github.com>
2025-05-02 20:31:12 -0300

    Implementação do método de atualizar produto

--- aplicativos/shop/lib/models/product_list.dart
+++ aplicativos/shop/lib/models/product_list.dart
@@ -7,8 +7,8 @@ import 'package:http/http.dart' as http;
 
 class ProductList with ChangeNotifier {
   final List<Product> _items = [];
-  final String _url =
-      "https://shop-51472-default-rtdb.firebaseio.com/products.json";
+  final String _baseUrl =
+      "https://shop-51472-default-rtdb.firebaseio.com/products";
 
   List<Product> get items => [..._items];
   List<Product> get favoriteItems =>
@@ -20,7 +20,7 @@ class ProductList with ChangeNotifier {
 
   Future<void> loadProducts() async {
     _items.clear();
-    final response = await http.get(Uri.parse(_url));
+    final response = await http.get(Uri.parse("$_baseUrl.json"));
     if (response.body == 'null') return;
     Map<String, dynamic> data = jsonDecode(response.body);
     data.forEach((productId, productData) {
@@ -40,7 +40,7 @@ class ProductList with ChangeNotifier {
 
   Future<void> addProduct({required Product product}) async {
     final response = await http.post(
-      Uri.parse("$_url"),
+      Uri.parse("$_baseUrl.json"),
       body: jsonEncode(
         {
           "name": product.title,
@@ -64,10 +64,22 @@ class ProductList with ChangeNotifier {
     notifyListeners(); // Notifico os meus interessados
   }
 
-  Future<void> updateProduct({required Product product}) {
+  Future<void> updateProduct({required Product product}) async {
     final int index = _items.indexWhere((prod) => prod.id == product.id);
 
     if (index >= 0) {
+      await http.patch(
+        Uri.parse("$_baseUrl/${product.id}.json"),
+        body: jsonEncode(
+          {
+            "name": product.title,
+            "description": product.description,
+            "price": product.price,
+            "imageUrl": product.imageUrl,
+          },
+        ),
+      );
+
       _items[index] = product;
     }
     notifyListeners();
