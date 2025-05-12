6c337eed0a17d959eec6db97a67581f754c14b70
Davi Ferreira <123248131+DaviFerreira0106@users.noreply.github.com>
2025-05-12 13:27:43 -0300

    Implementação do FutureBuilder

--- aplicativos/shop/lib/pages/order_page.dart
+++ aplicativos/shop/lib/pages/order_page.dart
@@ -4,46 +4,36 @@ import 'package:shop/models/order_list.dart';
 import 'package:provider/provider.dart';
 import 'package:shop/components/order_widget.dart';
 
-class OrderPage extends StatefulWidget {
+class OrderPage extends StatelessWidget {
   const OrderPage({
     super.key,
   });
 
   @override
-  OrderPageState createState() {
-    return OrderPageState();
-  }
-}
-
-class OrderPageState extends State<OrderPage> {
-  bool _isLoadind = true;
-
-  @override
-  void initState() {
-    super.initState();
-    Provider.of<OrderList>(
-      context,
-      listen: false,
-    ).loadOrders().then((_) => setState(() => _isLoadind = false));
-  }
-
-  @override
   Widget build(BuildContext context) {
-    final OrderList orderList = Provider.of(context);
     return Scaffold(
       appBar: AppBar(
         title: Text("Meus Pedidos"),
       ),
-      body: _isLoadind
-          ? Center(
+      body: FutureBuilder(
+        future: Provider.of<OrderList>(context, listen: false).loadOrders(),
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return Center(
               child: CircularProgressIndicator(),
-            )
-          : ListView.builder(
-              itemCount: orderList.itemCount,
-              itemBuilder: (context, index) => OrderWidget(
-                order: orderList.listCart.elementAt(index),
+            );
+          } else {
+            return Consumer<OrderList>(
+              builder: (context, orderList, child) => ListView.builder(
+                itemCount: orderList.itemCount,
+                itemBuilder: (context, index) => OrderWidget(
+                  order: orderList.listCart.elementAt(index),
+                ),
               ),
-            ),
+            );
+          }
+        },
+      ),
       drawer: AppDrawer(),
     );
   }
