82efee2ddd5437b54f8dd4dc5a68d46facaa183f
Davi Ferreira <123248131+DaviFerreira0106@users.noreply.github.com>
2025-07-15 22:20:06 -0300

    Configuração da pagina de formulario

--- aplicativos/chat/lib/components/auth_form.dart
+++ aplicativos/chat/lib/components/auth_form.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:chat/models/auth_form_data.dart';
 
 class AuthForm extends StatefulWidget {
   const AuthForm({super.key});
@@ -8,31 +9,75 @@ class AuthForm extends StatefulWidget {
 }
 
 class AuthFormState extends State<AuthForm> {
+  final _authForm = AuthFormData();
+  final _formKey = GlobalKey<FormState>();
+
+  void _submit() {
+    final isValid = _formKey.currentState?.validate() ?? false;
+
+    if(!isValid) return;
+  }
+
   @override
   Widget build(BuildContext context) {
     return Card(
       margin: const EdgeInsets.all(20),
       child: Form(
+        key: _formKey,
         child: Padding(
           padding: const EdgeInsets.all(15.0),
           child: Column(
             children: <Widget>[
+              if (_authForm.isSignUp)
+                TextFormField(
+                    key: ValueKey('name'),
+                    initialValue: _authForm.name,
+                    onChanged: (name) => _authForm.name = name,
+                    decoration: InputDecoration(
+                      label: Text('Nome'),
+                    ),
+                    validator: (_name) {
+                      final String name = _name ?? '';
+
+                      if (name.trim().length < 5) {
+                        return 'O nome precisa ser maior de 5 caracteres.';
+                      }
+
+                      return null;
+                    }),
               TextFormField(
-                decoration: InputDecoration(
-                  label: Text('Nome'),
-                ),
-              ),
+                  key: ValueKey('email'),
+                  initialValue: _authForm.email,
+                  onChanged: (email) => _authForm.email = email,
+                  decoration: InputDecoration(
+                    label: Text('E-mail'),
+                  ),
+                  validator: (_email) {
+                    final String email = _email ?? '';
+
+                    if (!email.contains('@')) {
+                      return 'E-mail inválido.';
+                    }
+
+                    return null;
+                  }),
               TextFormField(
-                decoration: InputDecoration(
-                  label: Text('E-mail'),
-                ),
-              ),
-              TextFormField(
-                obscureText: true,
-                decoration: InputDecoration(
-                  label: Text('Senha'),
-                ),
-              ),
+                  key: ValueKey('password'),
+                  initialValue: _authForm.password,
+                  onChanged: (password) => _authForm.password = password,
+                  obscureText: true,
+                  decoration: InputDecoration(
+                    label: Text('Senha'),
+                  ),
+                  validator: (_password) {
+                    final String password = _password ?? '';
+
+                    if (password.trim().length < 6) {
+                      return 'A senha precisa conter 6 caracteres.';
+                    }
+
+                    return null;
+                  }),
               SizedBox(height: 10),
               ElevatedButton(
                 style: ButtonStyle(
@@ -40,16 +85,20 @@ class AuthFormState extends State<AuthForm> {
                     Theme.of(context).buttonTheme.colorScheme?.primary,
                   ),
                 ),
-                onPressed: () {},
+                onPressed: _submit,
                 child: Text(
-                  'Entrar',
+                  _authForm.isLogin ? 'Entrar' : 'Cadastrar',
                   style: TextStyle(color: Colors.white),
                 ),
               ),
               SizedBox(height: 10),
               TextButton(
-                onPressed: () {},
-                child: Text('Criar Conta'),
+                onPressed: () {
+                  setState(() => _authForm.toggleAuthMode());
+                },
+                child: Text(
+                  _authForm.isLogin ? 'Criar Conta?' : 'Já possui conta?',
+                ),
               ),
             ],
           ),
