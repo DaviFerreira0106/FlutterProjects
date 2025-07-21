793754315a4b7b3d44c1505f7109e7fa194e6a63
Davi Ferreira <123248131+DaviFerreira0106@users.noreply.github.com>
2025-07-21 20:09:47 -0300

    Criação das telas de mensagem

--- aplicativos/chat/lib/pages/chat_page.dart
+++ aplicativos/chat/lib/pages/chat_page.dart
@@ -1,22 +1,52 @@
 import 'package:flutter/material.dart';
 import 'package:chat/core/services/auth/auth_service.dart';
+import 'package:chat/components/messages.dart';
+import 'package:chat/components/new_message.dart';
 
 class ChatPage extends StatelessWidget {
   const ChatPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text('Chat Page'),
-            TextButton(
-              onPressed: AuthServices().logout,
-              child: Text('Sair'),
+    return SafeArea(
+      child: Scaffold(
+        appBar: AppBar(title: Text('Chat'), actions: <Widget>[
+          DropdownButton(
+            icon: Icon(
+              Icons.more_vert,
+              color: Colors.black87,
             ),
-          ],
+            items: [
+              DropdownMenuItem(
+                value: 'logout',
+                child: SizedBox(
+                  child: Row(
+                    children: [
+                      Icon(Icons.exit_to_app),
+                      SizedBox(width: 10),
+                      Text('Sair'),
+                    ],
+                  ),
+                ),
+              ),
+            ],
+            onChanged: (value) {
+              if (value == 'logout') {
+                AuthServices().logout();
+              }
+            },
+          ),
+        ]),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Expanded(
+                child: Messages(),
+              ),
+              NewMessage(),
+            ],
+          ),
         ),
       ),
     );
