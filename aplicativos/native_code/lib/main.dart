59a9eec854a193fffd21bbbc1e92564c3a3206fb
Davi Ferreira da Silva de Oliveira <daviferreira@MacBook-Air-de-Davi.local>
2025-12-29 13:36:39 -0300

    Criação do projeto native code

---
+++ aplicativos/native_code/lib/main.dart
@@ -0,0 +1,60 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(const NativeCodeApp());
+
+class NativeCodeApp extends StatefulWidget {
+  const NativeCodeApp({super.key});
+
+  @override
+  NativeCodeAppState createState() => NativeCodeAppState();
+}
+
+class NativeCodeAppState extends State<NativeCodeApp> {
+  int _a = 0;
+  int _b = 0;
+  int _sum = 0;
+
+  void _calcSum() {
+    setState(() {
+      _sum = _a + _b;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(title: Text('Código Nativo')),
+        body: Center(
+          child: Padding(
+            padding: EdgeInsetsGeometry.all(20),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text('Soma: $_sum'),
+                TextField(
+                  keyboardType: TextInputType.number,
+                  onChanged: (value) {
+                    setState(() {
+                      _a = int.tryParse(value) ?? 0;
+                    });
+                  },
+                ),
+                TextField(
+                  keyboardType: TextInputType.number,
+                  onChanged: (value) {
+                    setState(() {
+                      _b = int.tryParse(value) ?? 0;
+                    });
+                  },
+                ),
+                SizedBox(height: 20),
+                ElevatedButton(onPressed: _calcSum, child: Text('Somar')),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
