e2bc8c09c6374d947eb067666da3cf36d28c8673
Davi Ferreira da Silva de Oliveira <daviferreira0106@gmail.com>
2025-12-29 17:26:20 -0300

    Implementação do código nativo swift

--- aplicativos/native_code/ios/Runner/AppDelegate.swift
+++ aplicativos/native_code/ios/Runner/AppDelegate.swift
@@ -7,7 +7,28 @@ import UIKit
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
-    GeneratedPluginRegistrant.register(with: self)
-    return super.application(application, didFinishLaunchingWithOptions: launchOptions)
+      let controller : FlutterViewController = window!.rootViewController as! FlutterViewController
+      
+      let nativeChannel = FlutterMethodChannel(name: "davi.com.br/native_code", binaryMessenger: controller.binaryMessenger)
+      
+      nativeChannel.setMethodCallHandler({
+          (call: FlutterMethodCall, result: @escaping FlutterResult) -> Void in
+          
+          guard call.method == "calcSum" else{
+              result(FlutterMethodNotImplemented)
+              return
+          }
+          
+          if let args = call.arguments as? [String : Any],
+             let a = args["a"] as? Int,
+             let b = args["b"] as? Int {
+              result(a + b)
+          } else {
+              result(-1)
+          }
+      })
+      
+      GeneratedPluginRegistrant.register(with: self)
+      return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
 }
